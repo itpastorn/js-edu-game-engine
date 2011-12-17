0bb7c12be8809037b30c141bfe39d8244e3c106d
Lars Gunther <gunther@keryx.se>
2011-12-17 22:42:49 +0100

    Initial idea for a JS game engine for edu
    This first commit is the state of 17 dec 2011
    I have made an initial main engine and small tests for HTML DOM, SVG and Canvas
    Lots of @todo in the code

---
+++ game-engine.php
@@ -0,0 +1,286 @@
+<?php
+/*
+ * Game engine that uses requestAnimationFrame
+ *
+ * move() is updated very often
+ * draw() only when needed
+ * 
+ * PHP only used for these comments (doc generation)
+ * @author Lars Gunther <gunther@keryx.se>
+ * @license MIT
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <title>Game engine idea</title>
+  <style>
+    #domball {
+        width: 10px;
+        height:10px;
+        background-color: blue;
+        border-radius: 50%;
+        position: absolute;
+        top: 1px;
+        left:1px;
+    }
+    #wall {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 150px;
+        height: 200px;
+        border: 1px solid;
+    }
+    #domballstart {
+        position: absolute;
+        top: 210px;
+    }
+    #cv {
+        width: 150px;
+        height: 200px;
+        position: absolute;
+        top: 0;
+        left: 200px;
+        border: 1px solid;
+    }
+    #cvstart {
+        position: absolute;
+        top: 210px;
+        left: 225px;
+    }
+    #svg {
+        width: 150px;
+        height: 200px;
+        position: absolute;
+        top: 0;
+        left: 400px;
+        border: 1px solid;
+    }
+    #svgballstart {
+        position: absolute;
+        top: 210px;
+        left: 425px;
+    }
+    #webgl {
+        
+    }
+    #webglstart {
+        
+    }
+    #log {
+        position: absolute;
+        top: 240px;
+        font-family: sans-serif;
+    }
+  </style>
+</head>
+<body>
+<div id="domball"></div>
+<div id="wall"></div>
+<button id="domballstart">HTML DOM ball</button>
+<canvas id="cv" width="150" height="200">Canvas ball demo</canvas>
+<button id="cvstart">Canvas ball</button>
+<svg id="svg" width="150" height="200" viewport="0 0 150 200">
+  <circle cx="5" cy="5" r="5" id="svgball" fill="red" />
+</svg>
+<button id="svgballstart">SVG ball</button>
+<div id="log"></div>
+<script src="kxengine.js"></script>
+<script>
+window.onerror = function (msg, url, line) {
+    var log = document.getElementById("log");
+    log.innerHTML = log.innerHTML += "<dl><dt>Error msg</dt><dd> " + msg + "</dd>\n";
+    log.innerHTML = log.innerHTML += "<dl><dt>Error url</dt><dd> " + url + "</dd>\n";
+    log.innerHTML = log.innerHTML += "<dl><dt>Error line</dt><dd> " + line + "</dd></dl>\n";
+}
+
+function log(msg) {
+    var log = document.getElementById("log");
+    log.innerHTML = log.innerHTML += "<dl><dt>Log</dt><dd> " + msg + "</dd>\n";
+}
+
+var domball = {
+    left: 1
+};
+domball.dom = document.getElementById("domball");
+
+// All math to collision detect, etc goes here
+// Move in memory and collision detect
+domball.move = function () {
+    if (domball.isMoving() === false) {
+    	return false;
+    }
+    // Stop moving when ball has returned to the top
+    if (domball.top > 0) {
+        // simulate a wall at the bottom
+        if (domball.top >= 190) {
+            domball.ySpeed = domball.ySpeed * -1;
+        }
+        domball.top += domball.ySpeed;
+        // Simulate a wall at 1px left and 151px left
+        if (domball.left < 1) {
+            domball.xSpeed = Math.abs(domball.xSpeed);
+        }
+        if (domball.left > 141) {
+            domball.xSpeed = -Math.abs(domball.xSpeed);
+        }
+        domball.left += domball.xSpeed;
+    } else {
+        domball.isMoving(false);
+    }
+}
+// Just update the screen, no math
+domball.draw = function () {
+    domball.dom.style.left = domball.left + "px";
+    domball.dom.style.top  = domball.top + "px";
+}
+domball.isMoving = function (toggle) {
+    if ( typeof toggle === "boolean" ) {
+        domball.moving = toggle;
+    }
+    return domball.moving;
+}
+
+// @todo Expand this and make canvsobjects a library of its own
+// Canvasobjects will inherit methods from this one
+var canvasobjects = {
+    setContext: function (id) {
+        canvasobjects.cvs = document.getElementById(id);
+        canvasobjects.ctx = canvasobjects.cvs.getContext('2d');
+        return canvasobjects.ctx;
+    },
+    // @todo Next one is not working, method should be set at this level (proto)
+    // but the moving property is specific to all objects 
+    // In order to fix this we must bind "this"...
+    isMoving : function (toggle) {
+        if ( typeof toggle === "boolean" ) {
+            canvasobjects.moving = toggle;
+        }
+        return canvasobjects.moving;
+    }
+    
+}
+canvasobjects.setContext('cv');
+
+canvasgame =  Object.create(canvasobjects);
+canvasball =  Object.create(canvasobjects);
+
+canvasgame.draw = function () {
+    canvasgame.ctx.clearRect(0, 0, 150, 200);
+}
+canvasball.draw = function () {
+    canvasball.ctx.fillStyle = "maroon";
+    canvasball.ctx.beginPath();
+    canvasball.ctx.arc(canvasball.x, canvasball.y, 5, 0, Math.PI * 2, true);
+    canvasball.ctx.closePath();
+    canvasball.ctx.fill();
+}
+// Start values
+canvasball.x = 6;
+canvasball.xSpeed = 4;
+
+canvasball.move = function () {
+    if (canvasball.isMoving() === false) {
+        return false;
+    }
+    // Stop moving when ball has returned to the top
+    if (canvasball.y > 4) {
+        // simulate a wall at the bottom
+        if (canvasball.y >= 195) {
+            canvasball.ySpeed = canvasball.ySpeed * -1;
+        }
+        canvasball.y += canvasball.ySpeed;
+        // Simulate a wall at 0 left and 150 left
+        if (canvasball.x < 6 || canvasball.x > 145) {
+            canvasball.xSpeed = canvasball.xSpeed * -1;
+        }
+        canvasball.x += canvasball.xSpeed;
+    } else {
+        canvasball.isMoving(false);
+    }
+}
+
+// Idea: Ball factory...
+
+// Handling SVG is almost identical to HTML DOM
+var svgball = {
+    cx: 1
+};
+svgball.dom = document.getElementById("svgball");
+
+// All math to collision detect, etc goes here
+// Move in memory and collision detect
+svgball.move = function () {
+    if (svgball.isMoving() === false) {
+        return false;
+    }
+    // Stop moving when ball has returned to the top
+    if (svgball.cy > 0) {
+        // simulate a wall at the bottom
+        if (svgball.cy >= 190) {
+            svgball.ySpeed = svgball.ySpeed * -1;
+        }
+        svgball.cy += svgball.ySpeed;
+        // Simulate a wall at 1px left and 151px left
+        if (svgball.cx < 1 || svgball.cx > 141) {
+            svgball.xSpeed = svgball.xSpeed * -1;
+        }
+        svgball.cx += svgball.xSpeed;
+    } else {
+        svgball.isMoving(false);
+    }
+}
+// Just update the screen, no math
+svgball.draw = function () {
+    svgball.dom.setAttribute("cx", svgball.cx);
+    svgball.dom.setAttribute("cy", svgball.cy);
+}
+svgball.isMoving = function (toggle) {
+    if ( typeof toggle === "boolean" ) {
+        svgball.moving = toggle;
+    }
+    return svgball.moving;
+}
+
+KXENGINE.setSpeed(200);
+KXENGINE.register(domball);
+KXENGINE.registerBackground(canvasgame);
+KXENGINE.register(canvasball);
+KXENGINE.register(svgball);
+
+document.getElementById("domballstart").onclick = function () {
+    canvasball.isMoving(false);
+    svgball.isMoving(false);
+    domball.isMoving(true);
+    domball.top    = 1;
+    domball.xSpeed = 4;
+    domball.ySpeed = 0.5;
+    KXENGINE.start();
+}
+
+document.getElementById("cvstart").onclick = function () {
+    domball.isMoving(false);
+    svgball.isMoving(false);
+    canvasgame.draw();
+    canvasball.y = 5;
+    canvasball.ySpeed = 0.5;
+    canvasball.move();
+    canvasball.draw();
+    canvasball.isMoving(true);
+    KXENGINE.start();
+}
+
+document.getElementById("svgballstart").onclick = function () {
+    canvasball.isMoving(false);
+    domball.isMoving(false);
+    svgball.isMoving(true);
+    svgball.cy     = 1;
+    svgball.xSpeed = 4;
+    svgball.ySpeed = 0.5;
+    KXENGINE.start();
+}
+</script>
+</body>
+</html>
---
+++ kxengine.js
@@ -0,0 +1,230 @@
+/**
+ * Simple game flow engine for new programmers
+ *
+ * Purpose: To be used for simple games in teaching
+ * Agnostic for HTML DOM, SVG, Canvas and WebGL
+ *
+ * Collision detection must be done in the moving objects
+ * This script is only the main controler
+ * 
+ * Usage:
+ * 1. Define background and movable objects according to interface
+ * 2. Register those objects
+ * 3. Set game speed (non mandatory step)
+ * 4. Call the start method
+ *
+ * @todo Make documentation and demos!
+ * @todo Rewrite using some smart pattern to make properties non public using ES5
+ * @todo "use strict";
+ *
+ * @license MIT (probably...)
+ */
+
+// @todo Capability check and polyfill requestAnimationFrame
+if ( typeof mozRequestAnimationFrame !== "function") {
+    throw new Error("Sorry, Firefox only during initial development");
+}
+
+var KXENGINE = {
+
+    // All moving objects that are handled by the engine goes here
+    // Protected
+    movables : [],
+
+    // The game "boards" - non-moving but may need updates
+    // Use with non retained graphics (Canvas, WebGL)
+    // Protected
+    backgrounds : [],
+    
+    // How often should in-memory updates be done
+    // Protected
+    movetime : 10,
+    
+    // Moving objects should be added to the main functions using this method
+    // @todo check that movable is an object that has all necessary methods (interface...?)
+    // Must have: obj.move(), obj.draw(), obj.isMoving()
+    register : function(object) {
+        if ( typeof object.movid === "number" ) {
+            throw new Error("Object " + object.movid + " already registered");
+        }
+        // More checks here...
+        var place = KXENGINE.movables.length;
+        KXENGINE.movables[place] = object;
+        object.movid = place;
+        return place;
+    },
+
+    // Non moving objects that may need to be refreshed for non retained graphics
+    registerBackground : function(object) {
+        // Perform checks here...
+        var place = KXENGINE.backgrounds.length;
+        KXENGINE.backgrounds[KXENGINE.movables.length] = object;
+        object.backgroundid = place;
+    },
+
+    // If an object is removed (as in space invaders ship destroyed) it should be
+    // un-registered
+    unRegister : function(object) {
+        // @todo investigate if we can keep this array dense
+        delete KXENGINE.movables[object.movid];
+        delete object.movid;
+        // @todo de-register backgrounds e.g. when reaching a new level
+    },
+    
+    // Start over
+    // @todo Investigate why it does not work
+    unRegisterAll : function() {
+        KXENGINE.movables = [];
+        KXENGINE.backgrounds = [];
+    },
+    
+    // Check if an object is registered
+    isRegistered : function(id) {
+        return typeof KXENGINE.movables[id] !== "undefined";
+        // @todo repeat for backgrounds
+    },
+
+    // This function updates movement in memory
+    move : function () {
+        var something_is_moving = false;
+        for (var i=0, len = KXENGINE.movables.length; i < len; i += 1) {
+            if ( typeof KXENGINE.movables[i] !== "undefined" && KXENGINE.movables[i].isMoving()) {
+                KXENGINE.movables[i].move();
+                something_is_moving = true;
+            }
+        }
+        if ( something_is_moving ) {
+            setTimeout(KXENGINE.move, KXENGINE.movetime);
+        } else {
+            // @todo remove this log
+            log("ALL movement stopped (reload to start again)");
+            // @todo Movements must be resumable
+        }
+        return something_is_moving;
+    },
+
+    // This function updates the screen (paints)
+    // To fix problems mentioned below we probably need 2 functions: One for retained mode and one for immediate mode graphics
+    draw : function () {
+        // @todo Fix logic problem. This loop seems to run once to clear the screen when when game is over.
+        for (var i=0, len = KXENGINE.backgrounds.length; i < len; i += 1) {
+            if ( typeof KXENGINE.backgrounds[i] !== "undefined" ) {
+                KXENGINE.backgrounds[i].draw();
+            }
+        }
+        // No need to repaint when nothing is moving
+        // Please note for canvas and WebGL that standing still = moving if you want it painted!
+        // @todo Must fix this discrepancy between retained mode and immediate mode graphics
+        var something_is_moving = false;
+        for (var i = 0, len = KXENGINE.movables.length; i < len; i += 1) {
+            if ( typeof KXENGINE.movables[i] !== "undefined" && KXENGINE.movables[i].isMoving()) {
+                KXENGINE.movables[i].draw();
+                something_is_moving = true;
+            }
+        }
+        if ( something_is_moving ) {
+            mozRequestAnimationFrame(KXENGINE.draw);
+            return true; // Something has moved
+        }
+        return false; // Nothing has moved
+    },
+    
+    // Sets movetime to change game speed
+    // De-coupled from screen updates!
+    setSpeed : function (fps) {
+        // Check that fps has an acceptable value
+        // @todo Capability detect max FPS
+        if ( typeof fps !== "number" ) {
+            throw new Error("Argument fps must be a number for KXENGINE.setSpeed()");
+        }
+        if ( fps < 10 || fps > 250 ) {
+            throw new Error("Argument fps must be between 10 and 250 for KXENGINE.setSpeed()");
+        }
+        if ( fps ) {
+            KXENGINE.movetime = 1000/fps;
+        } else {
+            KXENGINE.movetime = 10; // Reset to default
+        }
+        return KXENGINE.getSpeed();
+    },
+    
+    // Get frames per second for in memory movements
+    // Not for screen updates
+    getSpeed : function () {
+        return 1000 / KXENGINE.movetime;
+    },
+    
+    // Get frames per seconds for actual updates on screen
+    // Since these are decoupled from game engine math
+    // they do not measure game code quality 
+    getFps : function () {
+        throw new Error("Method getFps not implemented yet!");
+    },
+
+    // Pausing the game
+    // Returns an array with the movid of all paused objects
+    pause : function () {
+        throw new Error("Method pause() not implemented yet!");
+
+        var paused = [];
+        var pausnum = 0;
+        for (var i=0, len = KXENGINE.movables.length; i < len; i += 1) {
+            if ( typeof KXENGINE.movables[i] !== "undefined" && KXENGINE.movables[i].isMoving()) {
+                KXENGINE.movables[i].isMoving(false);
+                paused[pausnum] = KXENGINE.movables[i].movid;
+                pausnum += 1;
+            }
+        }
+        return paused;
+    },
+
+    // Unpausing the game
+    // Takes an array with all paused objects
+    // Returns true if at least one object has resumed moving
+    unpause : function (paused) {
+        throw new Error("Method unpause() not implemented yet!");
+
+        var something_is_moving = false;
+        for (var i=0, len = KXENGINE.paused.length; i < len; i += 1) {
+            KXENGINE.movables[i].isMoving(false);
+            something_is_moving = true;
+        }
+        return something_is_moving;
+    },
+    
+    // No start if already started...
+    running : false,
+    
+    // Begin the game!
+    start : function() {
+        if ( KXENGINE.movables.length < 1 ) {
+            throw new Error("No moving objects registered");
+        }
+        if ( !KXENGINE.running ) {
+            KXENGINE.running = true;
+            KXENGINE.move();
+            KXENGINE.draw();
+            return true;
+        }
+        return false;
+    }
+};
+
+
+/*
+Interfaces
+
+Necessary properties on a movable object
+o.move()             - Move in memory and do collision detection
+o.draw()             - Move on screen (paint)
+o.isMoving([toggle]) - when true o.move, and o.draw should be called
+                       when called with true movements starts or resumes
+                       when called with false movements stops
+o.movid              - to enable deRegister, e.g. when the object is destroyed
+
+If o.isMoving() returns false then neither obj.move(), nor obj.draw() will be called
+
+Necessary properties on a background objects
+o.draw()       - Should clear (part of) the screen and draw background items
+o.backgroundid - to enable deRegister
+*/
